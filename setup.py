807616e8971e7ff9b3fe3ec2499ab3dba25e0d9f
danielxwang90 <daniel.wang@oliverwyman.com>
2023-03-21 19:13:38 -0400

    Added new

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ with open('metadata.json') as fr:
     metadata = json.load(fr)
 
 setuptools.setup(
-    name="",  # Name of the repository
+    name="gryphon-pdf-data-extraction",  # Name of the repository
     version="0.0.1",
     author=metadata.get("author", ""),
     author_email=metadata.get("author_email", ""),
     description=metadata.get("description", ""),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",  # Repository URL or externally maintained page
+    url=metadata.get("read_more_link", ""),  # Repository URL or externally maintained page
     packages=setuptools.find_packages(),
     python_requires='>=3.7',
     install_requires=requirements,
